70b74c4232e0382750355db1de4fab3d159154d3
Mossahid123 <mossahidhossain309@gmail.comm>
2023-07-04 23:55:31 +0600

    appointment option component has created and loaded data on that component>

--- src/pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,10 +1,25 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentOption from './AppointmentOption';
 
-const AvailableAppointments = ({selectedDate}) => {
+const AvailableAppointments = ({ selectedDate }) => {
+    const [availableAppointment, setAvailableAppointment] = useState([]);
+
+    useEffect(() => {
+        fetch('appointmentOptions.json')
+            .then(res => res.json())
+            .then(data => setAvailableAppointment(data))
+    }, [])
     return (
-        <div className='mt-6'>
+        <div className='mt-16'>
             <p className='text-center text-secondary font-bold'>Available appointment on {format(selectedDate, 'PP')}</p>
+            <div>
+                { 
+                availableAppointment.map(option => <AppointmentOption
+                    key={option._id}
+                    AppointmentOption={option} />)
+                }
+            </div>
         </div>
     );
 };
