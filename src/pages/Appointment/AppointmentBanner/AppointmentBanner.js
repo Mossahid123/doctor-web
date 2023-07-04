7a45a095f7f14b1b526892ba2e2545c176ff0d75
Mossahid123 <mossahidhossain309@gmail.comm>
2023-07-04 22:29:16 +0600

    created a calender by using react daypicker

--- src/pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chair from '../../../assets/images/chair.png'
+import { DayPicker } from 'react-day-picker';
+import { format } from 'date-fns';
 
 const AppointmentBanner = () => {
+
+    const [selectedDate, setSelectedDate] = useState(new Date())
     return (
         <header>
             <div className="hero">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={chair} alt='chair' className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        
+                    <div className='mr-6'>
+                        <DayPicker
+                        mode= "single"
+                        selected={selectedDate}
+                        onSelect={setSelectedDate}
+                        />;
+                         <p>your selected date is :{format(selectedDate, 'PP')}</p>
                     </div>
+                   
                 </div>
             </div>
         </header>
